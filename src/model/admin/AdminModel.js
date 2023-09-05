f1b4b61d0d609096d22b856e9490aa51b9ed0070
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-09-05 20:25:22 +1000

    Updated adminModel

--- src/model/admin/AdminModel.js
+++ src/model/admin/AdminModel.js
@@ -24,6 +24,11 @@ export const updateAdminById = ({ _id, ...rest }) => {
   return AdminSchema.findByIdAndUpdate(_id, rest);
 };
 
+//@filter and @updateObj must ne an obj
+export const updateAdmin = (filter, updateObj) => {
+  return AdminSchema.findOneAndUpdate(filter, updateObj, { new: true });
+};
+
 export const deleteAdmin = (_id) => {
   return AdminSchema.findByIdAndDelete(_id);
 };
